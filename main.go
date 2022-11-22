8c0ccabfc5486d3c92501143f3d653d53ae33b0b
mdwitr0 <mdwit0r@gmail.com>
2022-11-22 21:35:31 +0100

    refactor(search): rename func

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	res, ok := search.LinearSearch(numbers, 6)
+	res, ok := search.Linear(numbers, 6)
 
 	if ok {
-		fmt.Println("Linear search result: ", res)
+		fmt.Println("Linear search result:", res)
 	}
 }
--- search/linear-search.go
+++ search/linear-search.go
@@ -1,7 +1,7 @@
 package search
 
-// LinearSearch имеет время выполнения O(n)
-func LinearSearch(list []int, x int) (res int, ok bool) {
+// Linear имеет время выполнения O(n)
+func Linear(list []int, x int) (res int, ok bool) {
 	for index, item := range list {
 		if item == x {
 			return index, true
